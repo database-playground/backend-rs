829053f4295009dc961cdce83f446cfaa97f1bbf
Yi-Jyun Pan <pan93412@gmail.com>
2024-08-25 02:50:44 +0800

    refactor(db): Simplify db code

--- src/db/event.rs
+++ src/db/event.rs
@@ -20,7 +20,7 @@ pub async fn create_attempt_event(
 ) -> Result<(), Error> {
     tracing::debug!("Creating attempt event in database");
 
-    let query = sqlx::query!(
+    sqlx::query!(
         r#"
         INSERT INTO dp_attempt_events (user_id, question_id, query, status)
         VALUES ($1, $2, $3, $4)
@@ -29,8 +29,9 @@ pub async fn create_attempt_event(
         question_id,
         query,
         status as AttemptStatus
-    );
-    conn.execute(query).await?;
+    )
+    .execute(conn)
+    .await?;
 
     Ok(())
 }
@@ -43,15 +44,16 @@ pub async fn create_solution_event(
 ) -> Result<(), Error> {
     tracing::debug!("Creating solution event in database");
 
-    let query = sqlx::query!(
+    sqlx::query!(
         r#"
         INSERT INTO dp_solution_events (user_id, question_id)
         VALUES ($1, $2)
         "#,
         user_id,
         question_id,
-    );
-    conn.execute(query).await?;
+    )
+    .execute(conn)
+    .await?;
 
     Ok(())
 }
