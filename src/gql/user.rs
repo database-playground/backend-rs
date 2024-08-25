db8182420e4d9f1c41eb6035180aefc7d2cd28a3
Yi-Jyun Pan <pan93412@gmail.com>
2024-08-26 03:24:36 +0800

    feat(gql): Allow getting group information

--- src/gql/user.rs
+++ src/gql/user.rs
@@ -1,4 +1,4 @@
-use async_graphql::{Context, Object, Result, SimpleObject};
+use async_graphql::{ComplexObject, Context, Object, Result, SimpleObject};
 use ecow::EcoString;
 
 use super::error::Error;
@@ -31,6 +31,7 @@ impl UserQuery {
 }
 
 #[derive(Debug, Clone, SimpleObject)]
+#[graphql(complex)]
 pub struct User {
     pub user_id: String,
     pub group_id: Option<i64>,
@@ -45,6 +46,43 @@ impl From<db::User> for User {
             group_id: user.group_id,
             created_at: user.created_at,
             updated_at: user.updated_at,
+        }
+    }
+}
+
+#[ComplexObject]
+impl User {
+    async fn group<'ctx>(&self, ctx: &Context<'ctx>) -> Result<Option<Group>> {
+        tracing::debug!("Running GraphQL query 'group'");
+
+        let Some(group_id) = self.group_id else {
+            return Ok(None);
+        };
+
+        let pool = ctx.data::<db::Pool>()?;
+        let group = db::get_group(pool, group_id).await?;
+
+        Ok(Some(group.into()))
+    }
+}
+
+#[derive(SimpleObject)]
+pub struct Group {
+    pub group_id: i64,
+    pub name: String,
+    pub description: String,
+    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub updated_at: chrono::DateTime<chrono::Utc>,
+}
+
+impl From<db::Group> for Group {
+    fn from(group: db::Group) -> Self {
+        Self {
+            group_id: group.group_id,
+            name: group.name,
+            description: group.description,
+            created_at: group.created_at,
+            updated_at: group.updated_at,
         }
     }
 }
