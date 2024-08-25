5519a1c1223c4a6d44be6634cd6b1a7a913b2379
Yi-Jyun Pan <pan93412@gmail.com>
2024-08-26 02:37:30 +0800

    test(fixture): Add group and user fixtures

---
+++ tests/fixtures/group.sql
@@ -0,0 +1,3 @@
+INSERT INTO dp_groups (name, description) VALUES ('group1', 'description1'),
+('group2', 'description2'),
+('group3', '');
---
+++ tests/fixtures/user.sql
@@ -0,0 +1,6 @@
+INSERT INTO dp_users (user_id, group_id) VALUES
+('usergeneric0', NULL),
+('usergeneric1', NULL),
+('usergroup1', 1),
+('usergroup2', 2),
+('usergroup3', 3);
